e997ef60a24769a6c28ac8ca2832a6e659515117
Vinicius Reis da Silva <viniciusreissilva808@gmail.com>
2023-10-03 08:55:35 -0300

    Integration with open.ai API in the create-transcription route to transform audio into text

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { createReadStream } from "fs";
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post('/videos/:videoId/transcription', async (req) => {
@@ -23,11 +25,28 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     })
 
     const videoPath = video.path
+    const audioReadStream = createReadStream(videoPath)
 
-    return {
-      videoId,
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: "whisper-1",
+      language: "pt",
+      response_format: "json",
+      temperature: 0,
       prompt,
-      videoPath,
-    }
+    })
+
+    const transcriptionIA = response.text
+
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription: transcriptionIA,
+      }
+    })
+
+    return { transcriptionIA }
   })
 }
